b4cebc752cde895e3c9ac74414b8cb8c5336597d
Nguyen Ninh Thuan <nguyenninhthuan00021102@gmail.com>
2022-06-30 21:41:13 +0700

    update the seperated page

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,43 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task AddViewCount(int productId);
+
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+
+        //Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        //Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        //Task AddViewcount(int productId);
+
+        //Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
+
+        //Task<int> AddImages(int productId, List<IFormFile> files);
+
+        //Task<int> RemoveImages(int imageId);
+
+        //Task<int> UpdateImage(int imageId, string caption, bool isDefault);
+
+        //Task<List<ProductImageViewModel>> GetListImage(int productId);
+    }
+}
